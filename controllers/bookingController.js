b9976dc415b532af4de44a498a8fb3c5ca59bc1a
Ishaque Ahmed <ishaqueahmednasim@gmail.com>
2022-09-02 17:10:58 +0600

    stripe -2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -35,7 +35,9 @@ exports.getCheckoutSession = async (req, res, next) => {
                         name: `${tour.name} Tour`,
                         description: tour.summary,
                         images: [
-                            `https://www.natours.dev/img/tours/${tour.imageCover}`,
+                            `${req.protocol}://${req.get('host')}/img/tours/${
+                                tour.imageCover
+                            }`,
                         ],
                     },
                 },
@@ -65,7 +67,7 @@ exports.getCheckoutSession = async (req, res, next) => {
 const createBookingsCheckout = async (session) => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.unit_amount / 100;
+    const price = session.amount_total / 100;
     await Booking.create({
         tour,
         user,
